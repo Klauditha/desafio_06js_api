72845044864b7376fccc1e5d145122e0ca0dd3c1
Claudia Villarroel <claudia.villarroel@live.com>
2023-08-21 21:39:55 -0400

    WIP: Maquetado

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,19 @@
-const getDatos = async() => {
+const urlApi = 'https://mindicador.cl/api/';
+
+const getDatosFile = async() => {
     const res = await fetch('mindicador.json');
     const data = await res.text();
     console.log(data);
 }
 
-getDatos();
+const getDatos = async() => {
+    const res = await fetch(urlApi);
+    const data = await res.json();
+    console.log(data);
+}
+
+const btnBuscar = document.getElementById("btnBuscar");
+btnBuscar.addEventListener("click", function(){
+    getDatos();
+})
+getDatosFile();
